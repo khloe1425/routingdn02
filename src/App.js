e4ed6658a09f1201f602f4e67cca1f6328c74fa2
Nguyen <tranchauthanhnguyen@gmail.com>
2022-04-21 22:11:36 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* Route: khai báo đường dẫn tới component */}
+      <Header/>
+      
+      <Switch>
+        <Route exact path="/help" render={(propsRoute) => { 
+            return<div>
+                <p>Help</p>
+                {/* <Help/> */}
+            </div>
+         }}/>
+
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+
+        <Route exact path="/" component={Home} />
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/redux/configStore.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore } from 'redux';
+
+const rootReducer = combineReducers({
+    //các reducer
+})
+
+export const store = createStore(rootReducer);
