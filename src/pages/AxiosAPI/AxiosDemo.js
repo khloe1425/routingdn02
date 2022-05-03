5ddd3c8e1acc5373bf97e5cda15c6c3beebcef59
Nguyen <tranchauthanhnguyen@gmail.com>
2022-05-03 19:09:40 +0700

    update

--- src/pages/AxiosAPI/AxiosDemo.js
+++ src/pages/AxiosAPI/AxiosDemo.js
@@ -3,31 +3,64 @@ import axios from 'axios'
 
 export default class AxiosDemo extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            mangPhim: []
+        }
+    }
 
     getMovieList = () => {
         let promise = axios({
             method: 'get',
             url: 'https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01',
-            headers:{
-                "TokenCybersoft":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwMiIsIkhldEhhblN0cmluZyI6IjI4LzA5LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2NDMyMzIwMDAwMCIsIm5iZiI6MTYzNzYwMDQwMCwiZXhwIjoxNjY0NDcwODAwfQ.VTPopaICbOzWIlCgYPhOFPrtszAvDaK0b8_0GZwdEAU"
+            headers: {
+                "TokenCybersoft": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwMiIsIkhldEhhblN0cmluZyI6IjI4LzA5LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2NDMyMzIwMDAwMCIsIm5iZiI6MTYzNzYwMDQwMCwiZXhwIjoxNjY0NDcwODAwfQ.VTPopaICbOzWIlCgYPhOFPrtszAvDaK0b8_0GZwdEAU"
             }
         });
 
-        promise.then((result) => { 
-            console.log(result.data.content);
-         })
+        promise.then((result) => {
+            //Lay data thanh cong
+            // console.log(result.data.content);
+            this.setState({
+                mangPhim: result.data.content
+            })
+        })
 
-         promise.catch((error) => { 
+        promise.catch((error) => {
             console.log(error);
-         })
+        })
     }
 
+    renderPhim = () => {
+        return this.state.mangPhim.map((phim) => {
+            return <div className="col-4" key={phim.maPhim}>
+                <div className="card">
+                    <img src={phim.hinhAnh} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{phim.tenPhim}</h5>
+                        <p className="card-text">{phim.moTa}</p>
+
+                    </div>
+                </div>
+
+            </div>
+        })
+    }
+
+
     render() {
+
         return (
-            <div>
-                <button onClick={() => { 
+            <div className='container'>
+                {console.log(this.state.mangPhim)}
+                <button onClick={() => {
                     this.getMovieList();
-                 }} className='btn btn-danger'>Click lay danh sach</button>
+                }} className='btn btn-danger'>Click lay danh sach</button>
+
+                <div className="row">
+                    {this.renderPhim()}
+                </div>
             </div>
         )
     }
